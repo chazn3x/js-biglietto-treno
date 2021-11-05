a822b3b8ae0e669e302b82ea69f65c44a684cb4b
Giovanni <gio.a.98@gmail.com>
2021-11-05 15:31:30 +0100

    controllo sui dati di input

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 // input
 
-const km = parseInt(prompt("Quanti chilometri vuoi percorrere?")), age = parseInt(prompt("Quanti anni hai?"));
+let km = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
+while ((isNaN(km)) || (km < 0)) {
+    km = parseInt(prompt("Inserisci un numero di chilometri valido:"));
+}
+let age = parseInt(prompt("Quanti anni hai?"));
+while ((isNaN(age)) || (age < 0) || (age > 120)) {
+    age = parseInt(prompt("Inserisci un'età valida:"));
+}
 let subtotal, discount = 0;
 
 // algoritmo
@@ -9,10 +16,11 @@ subtotal = 0.21 * km;
 if (age < 18) {
     discount = subtotal / 100 * 20;
 }
-if (age > 65) {
+if (age >= 65) {
     discount = subtotal / 100 * 40;
 }
 subtotal -= discount;
+subtotal = subtotal.toFixed(2);
 
 // output
 
